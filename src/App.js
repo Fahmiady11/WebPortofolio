5658c3643467981d70dd24d1144b1b814e7e7c10
fahmiady11 <asfahmi5@gmail.com>
2022-08-09 14:49:05 +0700

    change 4

--- src/App.js
+++ src/App.js
@@ -8,8 +8,19 @@ import useThemeStore from './store/ThemeStore';
 import 'animate.css';
 import Experience from './pages/Experince';
 import Error from './pages/Error';
+import React, { useState, useEffect } from 'react';
+import wajah from './assets/wajah.png';
+import Typed from 'react-typed';
 function App() {
   const theme = useThemeStore((state) => state.mode);
+  const [isLoad, setLoad] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoad(false);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <div
@@ -19,15 +30,39 @@ function App() {
             : 'bg-slate-900 text-white'
         }`}
       >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skill" element={<Skill />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/*" element={<Error />} />
-        </Routes>
+        {isLoad ? (
+          <div className="bg-white absolute w-full h-full flex justify-center items-center">
+            <div className='max-w-xs w-full h-auto flex justify-center items-center flex-col'>
+              <img src={wajah} alt="" className='w-1/2 drop-shadow-md'/>
+              <p className='font-poppins mt-3 font-bold text-lg text-[#332222]'>
+              <Typed
+                loop
+                typeSpeed={70}
+                backSpeed={70}
+                strings={["Loading..."]}
+                smartBackspace
+                shuffle={false}
+                backDelay={1}
+                fadeOut={false}
+                fadeOutDelay={200}
+                loopCount={0}
+                showCursor
+                cursorChar="|"
+              />
+              </p>
+            </div>
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/skill" element={<Skill />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/*" element={<Error />} />
+          </Routes>
+        )}
       </div>
     </>
   );
