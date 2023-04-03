75ed7da0b17cd45b1259f99bb189b8f13139b166
Fahmiady11 <asfahmi5@gmail.com>
2023-04-03 09:49:19 +0700

    add experience

--- src/components/molecules/ListExperience.js
+++ src/components/molecules/ListExperience.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ItemExperience from './ItemExperience';
+import React from "react";
+import ItemExperience from "./ItemExperience";
 
 export default function ListExperience(second) {
   return (
@@ -7,10 +7,15 @@ export default function ListExperience(second) {
       <div className="w-auto md:w-11/12">
         <ol className="relative border-l border-gray-200 dark:border-gray-700">
           <ItemExperience
+            title="PART TIME WEBSITE LANJALAN UTM"
+            date="December 2022 - Present"
+            decs="Working on Website Interfaces such as Traveloka with coverage of tourism, culinary, lodging, transportation, crafts in the Madura region."
+            latest={true}
+          />
+          <ItemExperience
             title="Front End Development Intern"
             date="September 2022 - December 2022"
             decs="In the Bangbeli Internship Program, internal participants will be faced with Project-Based learning methods, work on real projects at Bangbeli and are responsible for solving these problems, get enrichment materials from companies to develop skills, and get regular guidance from mentors."
-            latest={true}
           />
           <ItemExperience
             title="Front End Engineering Bootcamp Student"
